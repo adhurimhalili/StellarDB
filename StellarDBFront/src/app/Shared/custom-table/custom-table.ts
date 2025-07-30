36d8b0aba872e1dbb83af540d3c37f15be5ed8ff
FisnikHyseni <fh51748@ubt-uni.net>
2025-07-30 16:35:14 +0200

    ;

--- StellarDBFront/src/app/Shared/custom-table/custom-table.ts
+++ StellarDBFront/src/app/Shared/custom-table/custom-table.ts
@@ -16,7 +16,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
   styleUrl: './custom-table.css',
   imports: [CommonModule, MatTableModule, MatCardModule, MatIconModule, MatButtonModule, MatPaginatorModule, MatProgressSpinnerModule, MatMenuModule, MatSortModule],
 })
-//test
+
 export class CustomTable implements OnDestroy, OnChanges, AfterViewInit {
   // Inputs
   @Input() columns: { columnDef: string; header: string; cell?: (element: any) => string; cssClass?: string; }[] = [];
